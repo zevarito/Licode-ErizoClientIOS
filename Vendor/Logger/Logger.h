bb1251ef86959da87393822ba9a8442bd940a08d
Alvaro Gil <zevarito@gmail.com>
2015-10-26 21:51:16 -0200

    improve logger
    * allow use of mask logging levels per file.

--- Vendor/Logger/Logger.h
+++ Vendor/Logger/Logger.h
@@ -8,25 +8,42 @@
 #import <Foundation/Foundation.h>
 
 #ifdef DEBUG
-    #define L_DEBUG(f, ...) { \
-        [[Logger sharedInstance] log:[Logger sharedInstance].debugPrefix file:@(__FILE__) line:@(__LINE__) format:f , ##__VA_ARGS__]; \
-    }
+#define L_DEBUG(f, ...) { \
+logThis(LOG_MODE_DEBUG, @(__FILE__), @(__LINE__), f, ##__VA_ARGS__); \
+}
 #else
-    #define L_DEBUG(f, ...) {}
+#define L_DEBUG(f, ...) {}
 #endif
 
-
 #define L_ERROR(f, ...) { \
-[[Logger sharedInstance] log:[Logger sharedInstance].errorPrefix file:@(__FILE__) line:@(__LINE__) format:f , ##__VA_ARGS__]; \
+logThis(LOG_MODE_ERROR, @(__FILE__), @(__LINE__), f, ##__VA_ARGS__); \
 }
 
 #define L_WARNING(f, ...) { \
-[[Logger sharedInstance] log:[Logger sharedInstance].warningPrefix file:@(__FILE__) line:@(__LINE__) format:f , ##__VA_ARGS__]; \
+logThis(LOG_MODE_WARNING, @(__FILE__), @(__LINE__), f, ##__VA_ARGS__); \
 }
 
 #define L_INFO(f, ...) { \
-[[Logger sharedInstance] log:[Logger sharedInstance].infoPrefix file:nil line:nil format:f , ##__VA_ARGS__]; \
+logThis(LOG_MODE_INFO, nil, nil, f, ##__VA_ARGS__); \
 }
+
+@class Logger;
+
+typedef enum {
+    LOG_MODE_UNKNOWN,
+    LOG_MODE_DEBUG,
+    LOG_MODE_INFO,
+    LOG_MODE_WARNING,
+    LOG_MODE_ERROR
+} LOG_MODE;
+
+typedef NS_OPTIONS(NSUInteger, LOG_MODE_MASK) {
+    LOG_MODE_DEBUG_MASK         = 1 << LOG_MODE_DEBUG,
+    LOG_MODE_INFO_MASK          = 1 << LOG_MODE_INFO,
+    LOG_MODE_WARNING_MASK       = 1 << LOG_MODE_WARNING,
+    LOG_MODE_ERROR_MASK         = 1 << LOG_MODE_ERROR
+};
+
 
 @interface Logger : NSObject
 
@@ -41,15 +58,28 @@
 /// @name Properties
 ///-----------------------------------
 
-@property NSString *infoPrefix;
-@property NSString *debugPrefix;
-@property NSString *warningPrefix;
-@property NSString *errorPrefix;
+@property LOG_MODE_MASK logModes;
+@property NSMutableArray *prefixes;
 
 ///-----------------------------------
 /// @name Methods
 ///-----------------------------------
 
-- (void)log:(NSString *)prefix file:(NSString *)file line:(NSNumber *)line format:(NSString *)format, ...;
+- (void)log:(LOG_MODE)mode file:(NSString *)file line:(NSNumber *)line format:(NSString *)format, ...;
+- (void)log:(LOG_MODE)mode file:(NSString *)file line:(NSNumber *)line format:(NSString *)format args:(va_list)args;
+- (void)logWithModesOverride:(LOG_MODE_MASK)overrideModes mode:(LOG_MODE)mode file:(NSString *)file line:(NSNumber *)line
+                      format:(NSString *)format args:(va_list)args;
 
 @end
+
+FOUNDATION_STATIC_INLINE void logThis(LOG_MODE mode, NSString *file, NSNumber *line, NSString *format, ...) {
+    va_list args;
+    va_start(args, format);
+#ifdef LOG_MODES
+    LOG_MODE_MASK overrideModes = LOG_MODES;
+    [[Logger sharedInstance] logWithModesOverride:overrideModes mode:mode file:file line:line format:format args:args];
+#else
+    [[Logger sharedInstance] log:mode file:file line:line format:format args:args];
+#endif
+    va_end(args);
+}
