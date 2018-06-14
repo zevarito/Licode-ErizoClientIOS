3f761dd5049452877f8f78e33dc8724818cf95e5
Li Lin <allenlinli@gmail.com>
2018-06-14 15:24:11 +0800

    feat: add NSError category ECSignalingChannel

---
+++ ErizoClient/rtc/NSError+ECSignalingChannel.h
@@ -0,0 +1,17 @@
+//
+//  ErizoClientIOS
+//
+//  Copyright (c) 2015 Alvaro Gil (zevarito@gmail.com).
+//
+//  MIT License, see LICENSE file for details.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSError (ECSignalingChannel)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ErizoClient/rtc/NSError+ECSignalingChannel.m
@@ -0,0 +1,13 @@
+//
+//  ErizoClientIOS
+//
+//  Copyright (c) 2015 Alvaro Gil (zevarito@gmail.com).
+//
+//  MIT License, see LICENSE file for details.
+//
+
+#import "NSError+ECSignalingChannel.h"
+
+@implementation NSError (ECSignalingChannel)
+
+@end
