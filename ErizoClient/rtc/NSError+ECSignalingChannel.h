501990ef5cdb2437fb489effab804072725da6ef
Li Lin <allenlinli@gmail.com>
2018-06-14 15:48:19 +0800

    feat: add errors. Need to add error codes later.

--- ErizoClient/rtc/NSError+ECSignalingChannel.h
+++ ErizoClient/rtc/NSError+ECSignalingChannel.h
@@ -7,10 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ECStream.h"
+
+FOUNDATION_EXPORT NSString * const ECSignalingChannelErrorDomain;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSError (ECSignalingChannel)
+
++ (instancetype)ECSignalingChannelConnectErrorWithMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelPublishErrorWithMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelUnpublishErrorWithMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelSubscribeErrorWith:(NSString *)streamId withMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelUnsubscribeErrorWith:(NSString *)streamId withMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelSendTokenErrorWithMessage:(NSString *)errorMessage;
++ (instancetype)ECSignalingChannelWebsocketErrorWithMessage:(NSString *)errorMessage;
 
 @end
 
--- ErizoClient/rtc/NSError+ECSignalingChannel.m
+++ ErizoClient/rtc/NSError+ECSignalingChannel.m
@@ -8,6 +8,46 @@
 
 #import "NSError+ECSignalingChannel.h"
 
+NSString * const ECSignalingChannelErrorDomain = @"ECSignalingChannelErrorDomain";
+
 @implementation NSError (ECSignalingChannel)
+
++ (instancetype)ECSignalingChannelConnectErrorWithMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelPublishErrorWithMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelUnpublishErrorWithMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelSubscribeErrorWith:(NSString *)streamId withMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelUnsubscribeErrorWith:(NSString *)streamId withMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelSendTokenErrorWithMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
++ (instancetype)ECSignalingChannelWebsocketErrorWithMessage:(NSString *)errorMessage {
+    return [NSError ECSignalingChannelErrorWithErrorString:errorMessage code:0];
+}
+
+#pragma mark - Private Methods
+
++ (NSError *)ECSignalingChannelErrorWithErrorString:(NSString *)errorMessage code:(NSInteger)code {
+    NSDictionary * userInfo = @{NSLocalizedDescriptionKey: errorMessage};
+    NSError *error = [NSError errorWithDomain:ECSignalingChannelErrorDomain
+                                         code:code
+                                     userInfo:userInfo];
+    return error;
+}
 
 @end
